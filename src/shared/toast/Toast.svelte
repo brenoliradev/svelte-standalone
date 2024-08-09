cc58034ded5e60d2b2949646c3643311424eb006
Breno Lira <brenoliradev@gmail.com>
2024-08-09 17:33:02 -0300

    feat: optimize example toasts

--- src/shared/toast/Toast.svelte
+++ src/shared/toast/Toast.svelte
@@ -17,74 +17,71 @@
 >
 	{#if type === 'success'}
 		<svg
-			width="1.1em"
-			class="inline-block text-center"
-			aria-hidden="true"
-			focusable="false"
-			role="img"
-			xmlns="http://www.w3.org/2000/svg"
+			class="stroke-green-700"
+			fill="currentColor"
+			stroke-width="0"
 			viewBox="0 0 512 512"
+			width="1.3em"
+			xmlns="http://www.w3.org/2000/svg"
+			><path
+				fill="none"
+				stroke-miterlimit="10"
+				stroke-width="32"
+				d="M448 256c0-106-86-192-192-192S64 150 64 256s86 192 192 192 192-86 192-192z"
+			></path><path
+				fill="none"
+				stroke-linecap="round"
+				stroke-linejoin="round"
+				stroke-width="32"
+				d="M352 176 217.6 336 160 272"
+			></path></svg
 		>
-			<path
-				class="fill-green-900"
-				d="M256 8C119.033 8 8 119.033 8 256s111.033 248 248 248 248-111.033 248-248S392.967 8 256 8zm0 464c-118.664 0-216-96.055-216-216 0-118.663 96.055-216 216-216 118.664 0 216 96.055 216 216 0 118.663-96.055 216-216 216zm141.63-274.961L217.15 376.071c-4.705 4.667-12.303 4.637-16.97-.068l-85.878-86.572c-4.667-4.705-4.637-12.303.068-16.97l8.52-8.451c4.705-4.667 12.303-4.637 16.97.068l68.976 69.533 163.441-162.13c4.705-4.667 12.303-4.637 16.97.068l8.451 8.52c4.668 4.705 4.637 12.303-.068 16.97z"
-			/>
-		</svg>
 	{:else if type === 'error'}
 		<svg
-			width="1.1em"
-			class="inline-block text-center"
-			aria-hidden="true"
-			focusable="false"
-			role="img"
+			class="fill-red-700"
+			stroke="currentColor"
+			stroke-width="0"
+			viewBox="0 0 24 24"
+			width="1.3em"
 			xmlns="http://www.w3.org/2000/svg"
-			viewBox="0 0 512 512"
+			><path
+				d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM11 15H13V17H11V15ZM11 7H13V13H11V7Z"
+			></path></svg
 		>
-			<path
-				class="fill-red-700"
-				d="M256 40c118.621 0 216 96.075 216 216 0 119.291-96.61 216-216 216-119.244 0-216-96.562-216-216 0-119.203 96.602-216 216-216m0-32C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm-11.49 120h22.979c6.823 0 12.274 5.682 11.99 12.5l-7 168c-.268 6.428-5.556 11.5-11.99 11.5h-8.979c-6.433 0-11.722-5.073-11.99-11.5l-7-168c-.283-6.818 5.167-12.5 11.99-12.5zM256 340c-15.464 0-28 12.536-28 28s12.536 28 28 28 28-12.536 28-28-12.536-28-28-28z"
-			></path>
-		</svg>
 	{:else}
 		<svg
-			width="1.1em"
-			class="inline-block text-center"
-			aria-hidden="true"
-			focusable="false"
-			role="img"
+			class="fill-yellow-600"
+			stroke="currentColor"
+			stroke-width="0"
+			viewBox="0 0 24 24"
+			width="1.3em"
 			xmlns="http://www.w3.org/2000/svg"
-			viewBox="0 0 512 512"
+			><path
+				d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM11 15H13V17H11V15ZM11 7H13V13H11V7Z"
+			></path></svg
 		>
-			<path
-				class="fill-yellow-500"
-				d="M256 40c118.621 0 216 96.075 216 216 0 119.291-96.61 216-216 216-119.244 0-216-96.562-216-216 0-119.203 96.602-216 216-216m0-32C119.043 8 8 119.083 8 256c0 136.997 111.043 248 248 248s248-111.003 248-248C504 119.083 392.957 8 256 8zm-36 344h12V232h-12c-6.627 0-12-5.373-12-12v-8c0-6.627 5.373-12 12-12h48c6.627 0 12 5.373 12 12v140h12c6.627 0 12 5.373 12 12v8c0 6.627-5.373 12-12 12h-72c-6.627 0-12-5.373-12-12v-8c0-6.627 5.373-12 12-12zm36-240c-17.673 0-32 14.327-32 32s14.327 32 32 32 32-14.327 32-32-14.327-32-32-32z"
-			/>
-		</svg>
 	{/if}
 
-	<div class="ml-4 mr-auto">
+	<div class="ml-4 mr-auto max-w-72">
 		<slot />
 	</div>
 
 	{#if dismissible}
 		<button
-			class="m-0 border-none bg-transparent p-0 text-gray-600"
+			class="m-0 border-none bg-transparent p-0 text-gray-600 hover:opacity-80"
 			on:click={() => dispatch('dismiss')}
 		>
 			<svg
-				width="0.8em"
-				aria-hidden="true"
-				focusable="false"
-				role="img"
+				stroke="currentColor"
+				fill="currentColor"
+				stroke-width="0"
+				viewBox="0 0 512 512"
+				width="0.9em"
 				xmlns="http://www.w3.org/2000/svg"
-				viewBox="0 0 352 512"
-				class="inline-block text-center"
+				><path
+					d="m289.94 256 95-95A24 24 0 0 0 351 127l-95 95-95-95a24 24 0 0 0-34 34l95 95-95 95a24 24 0 1 0 34 34l95-95 95 95a24 24 0 0 0 34-34z"
+				></path></svg
 			>
-				<path
-					fill="currentColor"
-					d="M242.72 256l100.07-100.07c12.28-12.28 12.28-32.19 0-44.48l-22.24-22.24c-12.28-12.28-32.19-12.28-44.48 0L176 189.28 75.93 89.21c-12.28-12.28-32.19-12.28-44.48 0L9.21 111.45c-12.28 12.28-12.28 32.19 0 44.48L109.28 256 9.21 356.07c-12.28 12.28-12.28 32.19 0 44.48l22.24 22.24c12.28 12.28 32.2 12.28 44.48 0L176 322.72l100.07 100.07c12.28 12.28 32.2 12.28 44.48 0l22.24-22.24c12.28-12.28 12.28-32.19 0-44.48L242.72 256z"
-				/>
-			</svg>
 		</button>
 	{/if}
 </article>
