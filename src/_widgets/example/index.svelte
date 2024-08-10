669a84c7895eb4bdd574f8241311ef2daf7f3688
Breno Lira <brenoliradev@gmail.com>
2024-08-09 23:00:45 -0300

    feat: better css for example buttons

--- src/_widgets/example/index.svelte
+++ src/_widgets/example/index.svelte
@@ -9,20 +9,20 @@
 	console.log(props);
 </script>
 
-<div class="d-flex gap-3">
+<div class="d-flex gap-3 p-2">
 	<button
 		on:click|preventDefault={() => toast.success(props.success)}
-		class="rounded-md border-2 border-black bg-gray-400 px-3 py-2 text-black shadow-md"
+		class="rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 		>generate success toast</button
 	>
 	<button
 		on:click|preventDefault={() => toast.error(props.error)}
-		class="rounded-md border-2 border-black bg-gray-400 px-3 py-2 text-black shadow-md"
+		class="rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 		>generate error toast</button
 	>
 	<button
 		on:click|preventDefault={() => toast.info(props.info)}
-		class="rounded-md border-2 border-black bg-gray-400 px-3 py-2 text-black shadow-md"
+		class="rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
 		>generate info toast</button
 	>
 </div>
