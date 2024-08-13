d62249d0ea8dd5e2298d3b5e50023423731d81e2
Breno Lira <brenoliradev@gmail.com>
2024-08-13 10:46:00 -0300

    chore[tests]: toasts.spec.ts

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -45,7 +45,7 @@ const configs = embedFiles.map((file) => {
 	return {
 		input: file,
 		output: {
-			format: 'iife',
+			format: 'esm',
 			file: `${outputDir}.min.js`,
 			sourcemap: false
 		},
