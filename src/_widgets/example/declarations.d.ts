428238944928f6aa586382905fdd9e4205f284b2
Breno Lira <brenoliradev@gmail.com>
2024-09-23 22:44:27 -0300

    chore: remove unused declare class

--- src/_widgets/example/declarations.d.ts
+++ src/_widgets/example/declarations.d.ts
@@ -2,5 +2,3 @@ import type { ComponentProps, SvelteComponent } from 'svelte';
 import Example from './index.svelte';
 
 export type ExampleProps = ComponentProps<Example>;
-
-declare class ExampleComponent extends SvelteComponent<Example> {}
