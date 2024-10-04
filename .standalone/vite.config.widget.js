73abb8badfdf215f2cfb62471ec98c08324c9838
Breno Lira <brenoliradev@gmail.com>
2024-10-04 15:24:18 -0300

    chore: reorder plugins / add strip

--- .standalone/vite.config.widget.js
+++ .standalone/vite.config.widget.js
@@ -1,24 +1,41 @@
-import { build } from 'vite';
-
-import { defineConfig } from 'vite';
+import { build, defineConfig } from 'vite';
 import { glob } from 'glob';
 import path from 'path';
 
 import autoprefixer from 'autoprefixer';
 import tailwindcss from 'tailwindcss';
-
 import { visualizer } from 'rollup-plugin-visualizer';
+import resolve from '@rollup/plugin-node-resolve';
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import cssnanoPlugin from 'cssnano';
+import { libInjectCss } from 'vite-plugin-lib-inject-css';
+import strip from 'rollup-plugin-strip';
 
 import tailwindConfig from '../tailwind.config.js';
 
-import { libInjectCss } from 'vite-plugin-lib-inject-css';
-
-import resolve from '@rollup/plugin-node-resolve';
-
-import { svelte } from '@sveltejs/vite-plugin-svelte';
-import cssnanoPlugin from 'cssnano';
-
 const embedFiles = glob.sync('src/_widgets/**/embed.ts');
+
+const getPostCSSPlugins = (purgeDir) => [
+	autoprefixer(),
+	tailwindcss({
+		...tailwindConfig,
+		content: [
+			`./${purgeDir}/*.{svelte,ts,js}`,
+			`./${purgeDir}/*/*.{svelte,ts,js}`,
+			'./src/shared/*/*.{svelte,ts,js}'
+		]
+	}),
+	cssnanoPlugin()
+];
+
+const commonPlugins = (outputDir, visualizerDir) => [
+	svelte(),
+	visualizer({
+		filename: `${visualizerDir}.status.html`,
+		title: `${outputDir} status`
+	}),
+	libInjectCss()
+];
 
 const configs = embedFiles.map((file) => {
 	const outputDir = path.dirname(file).replace('src/', '').replace('_widgets/', '');
@@ -26,25 +43,13 @@ const configs = embedFiles.map((file) => {
 		.dirname(file)
 		.replace('src', 'static')
 		.replace('_widgets', 'dist/visualizer');
-
 	const purgeDir = path.dirname(file).replace('embed.ts', '');
 
 	return defineConfig({
 		css: {
 			postcss: {
-				plugins: [
-					autoprefixer(),
-					tailwindcss({
-						...tailwindConfig,
-						content: [
-							`./${purgeDir}/*.{svelte,ts,js}`,
-							`./${purgeDir}/*/*.{svelte,ts,js}`,
-							'./src/shared/*/*.{svelte,ts,js}'
-						]
-					}),
-					cssnanoPlugin()
-				]
-			}
+				plugins: getPostCSSPlugins(purgeDir),
+			},
 		},
 		build: {
 			minify: 'terser',
@@ -52,31 +57,29 @@ const configs = embedFiles.map((file) => {
 			lib: {
 				formats: ['umd'],
 				entry: file,
-				name: outputDir
+				name: outputDir,
 			},
 			outDir: 'static/dist/widgets',
 			rollupOptions: {
 				output: {
 					chunkFileNames: 'chunks/[name].[hash].js',
 					assetFileNames: 'assets/[name][extname]',
-					entryFileNames: `${outputDir}.min.js`
+					entryFileNames: `${outputDir}.min.js`,
 				},
-				plugins: [resolve({ browser: true, dedupe: ['svelte'] })]
-			}
+				plugins: [
+					resolve({ browser: true, dedupe: ['svelte'] }),
+					strip({
+						functions: ['console.log', 'assert.*']
+					})
+				],
+			},
 		},
-		plugins: [
-			svelte(),
-			visualizer({
-				filename: `${visualizerDir}.status.html`,
-				title: `${outputDir} status`
-			}),
-			libInjectCss()
-		],
+		plugins: commonPlugins(outputDir, visualizerDir),
 		resolve: {
 			alias: {
-				'@': path.resolve(__dirname.replace('.standalone', ''), 'src')
-			}
-		}
+				'@': path.resolve(__dirname.replace('.standalone', ''), 'src'),
+			},
+		},
 	});
 });
 
