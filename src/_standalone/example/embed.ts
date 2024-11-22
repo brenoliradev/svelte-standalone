e13c9bae789b156486130800841ba432de8fc02d
Breno Lira <brenoliradev@gmail.com>
2024-11-22 12:06:08 -0300

    init: remove example to write 'beyonk-demo'

--- src/_standalone/example/embed.ts
+++
@@ -1,5 +0,0 @@
-import { embed } from 'standalone/embed';
-
-import Example from './index.svelte';
-
-embed(Example, 'example');
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,2 @@
 <div class="flex flex-col items-start gap-2 p-2">
-	<a
-		class="rounded-lg bg-blue-700 px-5 py-2.5 text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
-		href="/example">Redirect to example script</a
-	>
 </div>
