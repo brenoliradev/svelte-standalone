333d929b5f4314860c832319b3bcd01b59d2b2f0
Breno Lira <brenoliradev@gmail.com>
2024-11-22 17:51:16 -0300

    feat: embed types

--- .standalone/embed.ts
+++ .standalone/embed.ts
@@ -1,55 +1,59 @@
 import type { ComponentProps, SvelteComponent } from 'svelte';
 
 // default with import/export
-export function embed<T>(mount: T, name: string) {
-	var component;
+export function embed<T>(m: T, n: string) {
+	var c;
 
-	function start(props: ComponentProps<T extends SvelteComponent ? T : never>) {
-		if (!component) {
-			// @ts-expect-error
-			component = new mount({
-				target: document.body,
-				props: {
-					...props,
-					id: name
-				}
+	window[n] = {
+		start: (props: ComponentProps<T extends SvelteComponent ? T : never>) => {
+			if (!c) {
+				// @ts-expect-error
+				c = new m({
+					target: document.body,
+					props: {
+						...props,
+						id: n
+					}
+				});
+			}
+		},
+		stop: () => (c.$destroy(), (c = false))
+	};
+}
+
+export function embedMultiple(m: any, n: string) {
+	window[n] = {
+		start: <T>(props: T, target?: string) => {
+			const c = new m({
+				target: !target ? document.body : document.getElementById(target),
+				props: props
 			});
+
+			return {
+				stop: () => c.$destroy()
+			};
 		}
-	}
-
-	const stop = () => (component.$destroy(), (component = false));
-
-	window[name] = {
-		start,stop
-		
 	};
 }
 
-export function embedMultiple(mount: any, componentName: string, target?: string) {
-	function start<T>(props: T) {
-		const c = new mount({
-			target: !target ? document.body : document.getElementById(target),
-			props: props
-		});
+export const autoEmbedWithTarget = (mount: any) => {
+	const t = new URL((document.currentScript as HTMLScriptElement).src).searchParams.get('target');
 
-		return {
-			stop: () => c.$destroy()
-		};
-	}
-
-	window[componentName] = {
-		start
-	};
-}
-
-export const autoEmbedWithTarget = (mount: any) =>
-	new mount({
-		target: document.getElementById(
-			new URL((document.currentScript as HTMLScriptElement).src).searchParams.get('target')!
-		)
+	const c = new mount({
+		target: document.getElementById(t)
 	});
 
-export const autoEmbedOnBody = (mount: any, name: string) =>
-	new mount({
+	window[t] = {
+		stop: () => c.$destroy()
+	};
+};
+
+export const autoEmbedOnBody = (m: any, n: string) => {
+	const c = new m({
 		target: document.body
 	});
+
+	window[n] = {
+		stop: () => c.$destroy()
+	};
+};
