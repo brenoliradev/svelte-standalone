9009f3d8c1706b2d6c3077b28cec158974bbfbcd
Breno Lira <brenoliradev@gmail.com>
2024-11-24 19:09:36 -0300

    fix: try with __dirname

--- src/generate-plop.js
+++ src/generate-plop.js
@@ -9,8 +9,7 @@ const newLink = (componentName) =>
 	`<a class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800" href="/${componentName}">Redirect to ${componentName} script</a>\n`;
 
 // Initialize Plop
-const plopFile = path.resolve('./plopfile.cjs')
-const plop = await nodePlop(plopFile);
+const plop = await nodePlop(`${__dirname}/plopfile.cjs`);
 
 function isWebComponent(componentName) {
 	return componentName.includes('-');
